84947aa68dd3c89922e24f368e703d1ddbd06799
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-02-28 17:50:22 -0600

    Added support for mysql in the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+// import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import './App.css'
+import Axios from 'axios';
 
 import Posts from './components/Posts/Posts';
 
 const App = () => {
+  const [movieName, setMovieName] = useState('');
+  const [movieReview, setReview] = useState('');
+
+  const submitReview = () => {
+    Axios.post("http://localhost:5000/api/insert", 
+    {
+      movieName: movieName, 
+      movieReview: movieReview
+    }).then(() => {
+      alert('Sucessfully added');
+    });
+  };
+
   return (
-    <Container maxwidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Serving webpage</Typography>
-      </AppBar>
-    </Container>
+    // <Container maxwidth="lg">
+    //   <AppBar position="static" color="inherit">
+    //     <Typography variant="h2" align="center">Serving webpage</Typography>
+    //   </AppBar>
+    // </Container>
+    <div className="App">
+      <h1>CRUD Application</h1>
+
+      <div className="form">
+        <label>Movie Name</label>
+        <input type="text" name="movieName" onChange={(e) => {
+          setMovieName(e.target.value)
+        }} />
+        <label>Review</label>
+        <input type="text" name = "review" onChange={(e) => {
+          setReview(e.target.value)
+        }} />
+        <button onClick={submitReview} >Submit</button>
+      </div>
+    </div>
+    
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,17 +1,38 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import mysql from 'mysql';
 import cors from 'cors';
+
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+// const mysql = require('mysql');
 
-app.use('/posts', postRoutes);
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'DAMS'
+});
 
-app.use(bodyParser.json({message: "hello"}));
-app.use(bodyParser.urlencoded({message: "hello"}));
+connection.connect();
+
 app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/api/insert', (req, res) => {
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+    const sqlInsert = "INSERT into movie_reviews (movieName, movieReview) VALUES (?,?)"
+    connection.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(err);
+    })
+})
 
 const CONNECTION_URL = 'ENTER DB INFO';
 const PORT = process.env.port || 5000;

5eed192d01833b14f796f2c8f9d324df51863a2c
nberta <nberta99@gmail.com>
2021-03-06 10:52:04 -0600

    Removed remaining mongoose references. Cleared up null error in server index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import mysql from 'mysql';
 import cors from 'cors';
 
@@ -39,17 +38,11 @@ app.post('/api/insert', (req, res) => {
     const resource = req.body.resource;
     const sqlInsert = "INSERT into resources (donorName, resource) VALUES (?,?)"
     connection.query(sqlInsert, [donorName, resource], (err, result) => {
-        console.log(err);
+        if (err) { console.log(err); }
     })
 })
 
 const CONNECTION_URL = 'ENTER DB INFO';
 const PORT = process.env.port || 5000;
 
-//mongoose.connect(CONNECTION_URL, { useUrlParser: true, useUnifiedTopology: true })
-//  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-//  .catch((error) => console.log(error.message));
-
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-
-//mongoose.set('useFindAndModify', false);

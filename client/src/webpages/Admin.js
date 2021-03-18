2757c5d3e31d970cedf7efe0f9351a00feacb4ca
nberta <nberta99@gmail.com>
2021-03-18 18:00:36 -0500

    Added bootstrap to some aspects of website

--- client/src/webpages/Admin.js
+++ client/src/webpages/Admin.js
@@ -2,11 +2,25 @@
 //import DisasterForm from '../components/Forms/Disaster/DisasterForm';
 
 function AdminPage() {
-    return(
-        <div className="AdminPage">
-            <h1>Admin Page</h1>
-        </div>
-    )
+
+    var loggedin = false;
+
+    if (loggedin) {
+        return(
+            <div className="AdminPage">
+                <h1>Logged in Admin Page</h1>
+                
+            </div>
+        )
+    } else {
+        return(
+            <div className="AdminPage">
+                <h1>Admin Page</h1>
+                
+            </div>
+        )
+    }
+
 }
 
 export default AdminPage;

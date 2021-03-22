7c7418b81152815ae16181df8078278a39005a38
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-03-22 17:14:24 -0500

    Added routes for pledge and disaster

--- client/src/routes.js
+++ client/src/routes.js
@@ -22,6 +22,8 @@ import Register from "./views/examples/Register.js";
 import Login from "./views/examples/Login.js";
 import Tables from "./views/examples/Tables.js";
 import Icons from "./views/examples/Icons.js";
+import Pledge from "./components/Posts/Pledge/Pledge";
+import DisasterPage from "./webpages/Disaster";
 
 var routes = [
   {
@@ -76,6 +78,22 @@ var routes = [
     icon: "ni ni-circle-08 text-pink",
     component: Register,
     layout: "/auth",
+  },
+  {
+    path: "/pledge",
+    name: "Pledge",
+    invisible: false,
+    icon: "ni ni-circle-08 text-pink",
+    component: Pledge,
+    layout: "/admin",
+  },
+  {
+    path: "/disaster",
+    name: "Disaster",
+    invisible: false,
+    icon: "ni ni-circle-08 text-pink",
+    component: DisasterPage,
+    layout: "/admin",
   },
 ];
 export default routes;

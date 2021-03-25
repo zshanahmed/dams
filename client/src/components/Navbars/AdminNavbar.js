46740ccdc7e625267bf8cd8b004a9c0bcdffcab4
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2021-03-25 00:18:33 -0500

    Fixed navbar

--- client/src/components/Navbars/AdminNavbar.js
+++ client/src/components/Navbars/AdminNavbar.js
@@ -63,15 +63,6 @@ const AdminNavbar = (props) => {
             <UncontrolledDropdown nav>
               <DropdownToggle className="pr-0" nav>
                 <Media className="align-items-center">
-                  <span className="avatar avatar-sm rounded-circle">
-                    <img
-                      alt="..."
-                      src={
-                        require("../../assets/img/theme/team-4-800x800.jpg")
-                          .default
-                      }
-                    />
-                  </span>
                   <Media className="ml-2 d-none d-lg-block">
                     <span className="mb-0 text-sm font-weight-bold">
                       Jessica Jones

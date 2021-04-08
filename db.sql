d4188350dfb85c02eb5e1bc505340003e5d37118
nberta <nberta99@gmail.com>
2021-04-08 09:16:17 -0500

    Updated db.sql with table structures

--- db.sql
+++ db.sql
@@ -7,9 +7,21 @@ CREATE TABLE `disasters` (
   UNIQUE KEY `iddisasters_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `pledge` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `resourceID` int NOT NULL,
+  `quantity` float DEFAULT NULL,
+  `requestID` int DEFAULT NULL,
+  `expiration` date DEFAULT NULL,
+  `isValid` tinyint DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idpledge_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `resources` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `donorName` varchar(45) NOT NULL,
+  `unit` varchar(45) NOT NULL,
   `resource` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -23,6 +35,7 @@ CREATE TABLE `users` (
   `email` varchar(200) DEFAULT NULL,
   `mobile_num` varchar(500) DEFAULT NULL,
   `role` varchar(200) DEFAULT NULL,
+  `zip` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
